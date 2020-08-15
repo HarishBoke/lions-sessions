ad9fa6f8c6b2c37616a5df00054e7f6bc44919db
dhanuKokate <you@example.com>
2020-08-15 18:03:28 +0530

    Address review comments

--- filter-assingment/Dhanashri/js/filter.js
+++ filter-assingment/Dhanashri/js/filter.js
@@ -1,5 +1,17 @@
 
-    function ImageData(){
+function filterBtn() {
+    let btnEle = document.querySelector('#filterButton');
+    var buttonData = ['all', 'Cakes', 'Cupcakes', 'Sweets', 'Doughnuts'];
+
+    buttonData.forEach(item => {
+        btnEle.innerHTML += `<a href="#" class="btn btn-outline-secondary btn-black text-uppercase filter-btn m-2" id="${item}" onclick="filterButton()">${item}</a>`;
+    });
+}
+
+filterBtn();
+
+
+function ImageData(){
     
     const rowEle = document.querySelector('#storeItems');
 
@@ -7,69 +19,69 @@
         title: 'sweet item',
         image: 'img/sweets-1.jpeg',
         price: '5',
-        category: 'sweets'
+        category: 'Sweets'
     },{
         title: 'cupcake item',
         image: 'img/cupcake-1.jpeg',
         price: '5',
-        category: 'cupcakes'
+        category: 'Cupcakes'
     },{
         title: 'cake item',
         image: 'img/cake-1.jpeg',
         price: '5',
-        category: 'cakes'
+        category: 'Cakes'
     },{
         title: 'doughnut item',
         image: 'img/doughnut-1.jpeg',
         price: '5',
-        category: 'doughnuts'
+        category: 'Doughnuts'
     },{
         title: 'sweet item',
         image: 'img/sweets-2.jpeg',
         price: '10',
-        category: 'sweets'
+        category: 'Sweets'
     },{
         title: 'cupcake item',
         image: 'img/cupcake-2.jpeg',
         price: '10',
-        category: 'cupcakes'
+        category: 'Cupcakes'
     },{
         title: 'cake item',
         image: 'img/cake-2.jpeg',
         price: '10',
-        category: 'cakes'
+        category: 'Cakes'
     },{
         title: 'doughnut item',
         image: 'img/doughnut-2.jpeg',
         price: '10',
-        category: 'doughnuts'
+        category: 'Doughnuts'
     },{
         title: 'sweet item',
         image: 'img/sweets-3.jpeg',
         price: '15',
-        category: 'sweets'
+        category: 'Sweets'
     },{
         title: 'cupcake item',
         image: 'img/cupcake-3.jpeg',
         price: '15',
-        category: 'cupcakes'
+        category: 'Cupcakes'
     },{
         title: 'cake item',
         image: 'img/cake-3.jpeg',
         price: '15',
-        category: 'cakes'
+        category: 'Cakes'
     },{
         title: 'doughnut item',
         image: 'img/doughnut-3.jpeg',
         price: '15',
-        category: 'doughnuts'
+        category: 'Doughnuts'
     }];
 
-    for(let data of galleryData){
-        rowEle.innerHTML += `<div class="col-10 col-sm-6 col-lg-4 mx-auto my-3 filterEle" id="${data.category}">
+    galleryData.forEach(item => {
+        rowEle.innerHTML += `<div class="col-10 col-sm-6 col-lg-4 mx-auto my-3 filterEle ${item.category}" id="${item.category}">
                 <div class="card" >
                 <div class="img-container">
-                    <img src="${data.image}" class="card-img-top store-img" alt="">
+                    <img src="${item.image}" class="card-img-top store-img" alt="">
                     <span class="store-item-icon">
                     <i class="fas fa-shopping-cart"></i>
                     </span>
@@ -77,74 +89,50 @@
 
                 <div class="card-body">
                     <div class="card-text d-flex justify-content-between text-capitalize">
-                    <h5 id="store-item-name">${data.title}</h5>
-                    <h5 class="store-item-value">$ <strong id="store-item-price" class="font-weight-bold">${data.price}</strong></h5>
+                    <h5 id="store-item-name">${item.title}</h5>
+                    <h5 class="store-item-value">$ <strong id="store-item-price" class="font-weight-bold">${item.price}</strong></h5>
                     </div>
                 </div>
 
                 </div>
             </div>`;
-        }
-    }
-
-    ImageData();
-    
-    function filterBtn(data){
-    const filterEle = document.querySelectorAll('.filterEle');
-    hideAllEle();
-    if(data === 'all'){
-        filterEle.forEach(function(item){
-            if (item.id){
-                    item.style.display = 'block';
-                } 
-        }) ;  
-        }else if (data === 'cakes'){
-            filterEle.forEach(function(item){
-                if (item.id === 'cakes'){
-                    item.style.display = 'block';
-                }
-            });
-        }else if (data === 'cupcakes'){
-            filterEle.forEach(function(item){
-                if (item.id === 'cupcakes'){
-                    item.style.display = 'block';
-                }
-            });
-        }else if (data === 'sweets'){
-            filterEle.forEach(function(item){
-                if (item.id === 'sweets'){
-                    item.style.display = 'block';
-                }
-            });
-        }else if (data === 'doughnuts'){
-            filterEle.forEach(function(item){
-                if (item.id === 'doughnuts')){
-                    item.style.display = 'block';
-                }
-            });
-        }
-    }
-
-
-    function hideAllEle(){
-        let storeItems = document.querySelectorAll('.filterEle');
-
-        storeItems.forEach(function(item){
-            item.style.display = 'none';
         });
     }
 
-    function searchEle(){
-        const inputTxt = document.querySelector('#search-item').value;
-        const storeItems = document.querySelectorAll('.filterEle');
+    ImageData();
 
+ const storeItems = document.querySelectorAll('.filterEle');
 
-        storeItems.forEach((item) => {
-            if(item.id === inputTxt){
+ function filterButton() {
+
+    let data = event.target.id;
+
+        storeItems.forEach((item)=> {
+            if (data === 'all'){
                 item.style.display = 'block';
+            } else {
+                if (item.id == data){
+                    item.style.display = 'block';
+                }
+            }
+        })
+    }
+
+function hideAllEle(){
+    storeItems.forEach(function(item){
+        item.style.display = 'none';
+    });
+}
+    
+function searchEle(){
+    let inputTxt = document.querySelector('#searchItem').value;
+
+    storeItems.forEach((item) => {
+        if (item.textContent.includes(inputTxt)){
+            item.style.display = 'block';
         } else {
             item.style.display = 'none';
         }
-    });  
+    }); 
 
-    }
+}
