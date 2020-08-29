55c92ddf7f7805d66e0248e4d3dd89d663f4c77b
dhanuKokate <you@example.com>
2020-08-29 14:58:33 +0530

    As per requirement done few changes

--- star-rating/Dhanashri/script.js
+++ star-rating/Dhanashri/script.js
@@ -1,49 +1,70 @@
 const starWrapper = document.querySelector(".rating-ele");
 const starEle = document.querySelector(".rating-ele").children;
-const sratCount = 5;
+const startCount = 5;
 const ratingValue = document.querySelector("#ratingValue");
 const massage = ['Terrible', 'Poor', 'Average', 'Very Good', 'Excellent'];
 
- 
+
+let setAttributes = (element,attribute) => {
+    for(let key in attribute) {
+        element.setAttribute(key, attribute[key]);
+    }
+}
+
 const generateStarUI = () => {
     
-        for (let i = 0; i < sratCount; i++){
-            let starEle = document.createElement('span');
-            starEle.setAttribute('class', "fa fa-star-o");
+        for (let i = 0; i < startCount; i++){
+			let starEle = document.createElement('span');
+			setAttributes(starEle,{'class':'fa fa-star','id':i,'onclick':'getText(\'' + massage[i] + '\',\'' + i + '\')'});
             starWrapper.appendChild(starEle);
         } 
-}  
+} 
 
-const activeStar = () => {
-	
-	const eleAction = 'click';
-  
-	for(let i = 0; i < starEle.length; i++){
-		
-	
-		starEle[i].addEventListener(eleAction, function(){
-			
-			for(let j = 0; j < starEle.length; j++){
-				starEle[j].classList.remove("fa-star");		 	
-				starEle[j].classList.add("fa-star-o");		 	
-			}
-			
-			for(let j = 0; j <= i; j++){
-				starEle[j].classList.remove("fa-star-o");		 	
-				starEle[j].classList.add("fa-star");		 	
-			}
-			
-			ratingValue.innerHTML = massage[i];
-		})
-		
-		
-	}
+let allPreviousActive = [];
+
+// Get Previous Rating 
+let getPreviousRating = () => {
+    allPreviousActive = [];
+    for (let i = 0; i < starEle.length; i++) {     
+        if (starEle[i].classList.contains("active")) {
+            allPreviousActive.push(starEle[i]);
+        }        
+    } 
+    return allPreviousActive;
+}
+
+// Remove Rating 
+let removeRating = (totalLength) => {
+    for(let i=0; i<totalLength; i++){
+        starEle[i].classList.remove("active");
+    }
+}
+
+let  getText = (msg,ratingId) => {
+    
+    // Get Previous Active
+    let allPreviousActive = getPreviousRating();
+    
+    let relength = parseInt(ratingId)+parseInt(1);
+
+    if(allPreviousActive.length == relength){
+        removeRating(relength);
+        ratingValue.innerHTML = "";
+    }else{
+        removeRating(starEle.length);
+
+        for(let k=0;k<=ratingId;k++){
+            let element = document.getElementById(k);
+            element.classList.add("active");
+        }
+        ratingValue.innerHTML = msg;
+    }
+    
 }
 
 const init = () => {
 
 generateStarUI();
-activeStar();
 }
 init();
 
